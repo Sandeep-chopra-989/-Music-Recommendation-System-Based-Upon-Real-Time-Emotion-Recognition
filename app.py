6e2fae7add99e13917070980304c6ce6298fdaa2
Sandeep <148614245+Sandeep-chopra-989@users.noreply.github.com>
2025-04-24 19:16:29 +0530

    app.py

--- app.py
+++ app.py
@@ -3,151 +3,80 @@ import numpy as np
 import streamlit as st
 import cv2
 import pandas as pd
-
+import base64
 from collections import Counter
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPooling2D
-import base64
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
+# ------------------- Spotify Authentication --------------------
+SPOTIPY_CLIENT_ID = "8883f9995ff04d518f597bec4ce92e63"
+SPOTIPY_CLIENT_SECRET = "00160d0f2dbf4104abf3290ecc773188"
 
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id=SPOTIPY_CLIENT_ID,
+    client_secret=SPOTIPY_CLIENT_SECRET
+))
 
+# ------------------ Read and process dataset -------------------
 df = pd.read_csv("muse_v3.csv")
-
 df['link'] = df['lastfm_url']
 df['name'] = df['track']
 df['emotional'] = df['number_of_emotion_tags']
 df['pleasant'] = df['valence_tags']
-
-df = df[['name','emotional','pleasant','link','artist']]
-print(df)
-
+df = df[['name', 'emotional', 'pleasant', 'link', 'artist']]
 df = df.sort_values(by=["emotional", "pleasant"])
-df.reset_index()
-print(df)
+df.reset_index(drop=True, inplace=True)
 
+# Split into emotions
 df_sad = df[:18000]
 df_fear = df[18000:36000]
 df_angry = df[36000:54000]
 df_neutral = df[54000:72000]
 df_happy = df[72000:]
 
+# ---------------------- Helper Functions -----------------------
 def fun(list):
-
     data = pd.DataFrame()
-
-    if len(list) == 1:
-        v = list[0]
-        t = 30
-        if v == 'Neutral':
-            data = pd.concat([data, df_neutral.sample(n=t)], ignore_index=True)
-        elif v == 'Angry':
-             data = pd.concat([data, df_angry.sample(n=t)], ignore_index=True)
-        elif v == 'fear':
-            data = pd.concat([data, df_fear.sample(n=t)], ignore_index=True)
-        elif v == 'happy':
-            data = pd.concat([data, df_happy.sample(n=t)], ignore_index=True)
-        else:
-            data = pd.concat([data, df_angry.sample(n=t)], ignore_index=True)
-
-    elif len(list) == 2:
-        times = [30,20]
-        for i in range(len(list)):
-            v = list[i]
-            t = times[i]
-            if v == 'Neutral':
-                data = pd.concat([data, df_neutral.sample(n=t)], ignore_index=True)
-            elif v == 'Angry':    
-                data = pd.concat([data, df_angry.sample(n=t)], ignore_index=True)
-            elif v == 'fear':              
-                data = pd.concat([data, df_fear.sample(n=t)], ignore_index=True)
-            elif v == 'happy':             
-                data = pd.concat([data, df_happy.sample(n=t)], ignore_index=True)
-            else:              
-               data = pd.concat([df_sad.sample(n=t)])
-
-    elif len(list) == 3:
-        times = [55,20,15]
-        for i in range(len(list)): 
-            v = list[i]          
-            t = times[i]
-
-            if v == 'Neutral':              
-                data = pd.concat([data, df_neutral.sample(n=t)], ignore_index=True)
-            elif v == 'Angry':               
-                data = pd.concat([data, df_angry.sample(n=t)], ignore_index=True)
-            elif v == 'fear':             
-                data = pd.concat([data, df_fear.sample(n=t)], ignore_index=True)
-            elif v == 'happy':               
-                data = pd.concat([data, df_happy.sample(n=t)], ignore_index=True)
-            else:      
-                data = pd.concat([df_sad.sample(n=t)])
-
-
-    elif len(list) == 4:
-        times = [30,29,18,9]
-        for i in range(len(list)):
-            v = list[i]
-            t = times[i]
-            if v == 'Neutral': 
-                data = pd.concat([data, df_neutral.sample(n=t)], ignore_index=True)
-            elif v == 'Angry':              
-                data = pd.concat([data, df_angry.sample(n=t)], ignore_index=True)
-            elif v == 'fear':              
-                data = pd.concat([data, df_fear.sample(n=t)], ignore_index=True)
-            elif v == 'happy':               
-                data =pd.concat([data, df_happy.sample(n=t)], ignore_index=True)
-            else:              
-               data = pd.concat([df_sad.sample(n=t)])
-    else:
-        times = [10,7,6,5,2]
-        for i in range(len(list)):           
-            v = list[i]         
-            t = times[i]
-            if v == 'Neutral':
-                data = pd.concat([data, df_neutral.sample(n=t)], ignore_index=True)
-            elif v == 'Angry':           
-                data = pd.concat([data, df_angry.sample(n=t)], ignore_index=True)
-            elif v == 'fear':           
-                data = pd.concat([data, df_fear.sample(n=t)], ignore_index=True)
-            elif v == 'happy':          
-                data = pd.concat([data, df_happy.sample(n=t)], ignore_index=True)
-            else:
-                data = pd.concat([df_sad.sample(n=t)])
-
-    print("data of list func... :",data)
+    times_map = {
+        1: [30],
+        2: [30, 20],
+        3: [55, 20, 15],
+        4: [30, 29, 18, 9],
+        5: [10, 7, 6, 5, 2]
+    }
+    times = times_map.get(len(list), [10] * len(list))
+    for i, v in enumerate(list):
+        t = times[i]
+        if v == 'Neutral': data = pd.concat([data, df_neutral.sample(n=t)], ignore_index=True)
+        elif v == 'Angry': data = pd.concat([data, df_angry.sample(n=t)], ignore_index=True)
+        elif v == 'fear': data = pd.concat([data, df_fear.sample(n=t)], ignore_index=True)
+        elif v == 'happy': data = pd.concat([data, df_happy.sample(n=t)], ignore_index=True)
+        else: data = pd.concat([data, df_sad.sample(n=t)], ignore_index=True)
     return data
 
 def pre(l):
-
     emotion_counts = Counter(l)
-    result = []
-    for emotion, count in emotion_counts.items():
-        result.extend([emotion] * count)
-    print("Processed Emotions:", result)
-
-    # result = [item for items, c in Counter(l).most_common()
-    #           for item in [items] * c]
-
+    result = [emotion for emotion, count in emotion_counts.items() for _ in range(count)]
     ul = []
-    for x in result:
-        if x not in ul:
-            ul.append(x)
-            print(result)
-    print("Return the list of unique emotions in the order of occurrence frequency :",ul)
+    [ul.append(x) for x in result if x not in ul]
     return ul
-    
 
+def get_spotify_embed_link(track, artist):
+    query = f"{track} {artist}"
+    results = sp.search(q=query, type='track', limit=1)
+    if results['tracks']['items']:
+        track_url = results['tracks']['items'][0]['external_urls']['spotify']
+        embed_link = track_url.replace("open.spotify.com/track", "open.spotify.com/embed/track")
+        return embed_link
+    return None
 
-
-
+# ------------------ Load Face Detection Model ------------------
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
 model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
@@ -156,25 +85,16 @@ model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.5))
-
 model.add(Dense(7, activation='softmax'))
 
+model.load_weights("C:\\Users\\sande\\Downloads\\Project\\Emotion-based-music-recommendation-system-main\\Emotion-based-music-recommendation-system-main\\model.h5")
 
-model.load_weights('D:\\INTERNSHIP\\sem4\\Emotion-based-music-recommendation-system-main\\Emotion-based-music-recommendation-system-main\\model.h5')
-
-emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
-
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "fear", 3: "happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
 cv2.ocl.setUseOpenCL(False)
 cap = cv2.VideoCapture(0)
 
-print("Loading Haarcascade Classifier...")
-face = cv2.CascadeClassifier('D:\\INTERNSHIP\\sem4\\Emotion-based-music-recommendation-system-main\\Emotion-based-music-recommendation-system-main\\haarcascade_frontalface_default.xml')
-if face.empty():
-    print("Haarcascade Classifier failed to load.")
-else:
-    print("Haarcascade Classifier loaded successfully.")
-
+# -------------------------- Streamlit UI ------------------------
 page_bg_img = '''
 <style>
 body {
@@ -184,76 +104,55 @@ body {
 </style>
 '''
 st.markdown(page_bg_img, unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center; color: white'><b>Emotion based music recommendation</b></h2>"
-            , unsafe_allow_html=True)
-st.markdown("<h5 style='text-align: center; color: grey;'><b>Click on the name of recommended song to reach website</b></h5>"
-            , unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: white'><b>Emotion based music recommendation</b></h2>", unsafe_allow_html=True)
+st.markdown("<h5 style='text-align: center; color: grey;'><b>Click on the name of recommended song to reach website</b></h5>", unsafe_allow_html=True)
 
-col1,col2,col3 = st.columns(3)
+col1, col2, col3 = st.columns(3)
 
-list = []
-with col1:
-    pass
+emotion_list = []
+detected_emotion = ""  # Variable to store the latest detected emotion
 with col2:
     if st.button('SCAN EMOTION(Click here)'):
-
         count = 0
-        list.clear()
+        emotion_list.clear()
 
         while True:
-
             ret, frame = cap.read()
-            if not ret:
-                break
-            
-            face = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")          
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)        
-            faces = face.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)          
-            count = count + 1
+            if not ret: break
 
-            for (x, y, w, h) in faces:               
-                cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)               
-                roi_gray = gray[y:y + h, x:x + w]               
-                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)             
-                prediction = model.predict(cropped_img)             
+            face = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+            count += 1
+
+            for (x, y, w, h) in faces:
+                roi_gray = gray[y:y + h, x:x + w]
+                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+                prediction = model.predict(cropped_img)
                 max_index = int(np.argmax(prediction))
+                detected_emotion = emotion_dict[max_index]  # Update the detected emotion
+                emotion_list.append(detected_emotion)
+                cv2.putText(frame, detected_emotion, (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                            (255, 255, 255), 2, cv2.LINE_AA)
+                cv2.imshow('Video', cv2.resize(frame, (1000, 700)))
 
-                list.append(emotion_dict[max_index])
-
-                cv2.putText(frame, emotion_dict[max_index], (x + 20, y - 60), 
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)              
-                cv2.imshow('Video', cv2.resize(frame, (1000, 700), interpolation=cv2.INTER_CUBIC))
-
-            if cv2.waitKey(1) & 0xFF == ord('s'):
+            if cv2.waitKey(1) & 0xFF == ord('s') or count >= 20:
                 break
-            if count >= 20:
-                break
+
         cap.release()
         cv2.destroyAllWindows()
+        emotion_list = pre(emotion_list)
+        st.success(f"Emotions successfully detected! Current detected emotion: {detected_emotion}")  # Show the emotion
 
-        list = pre(list)
-        st.success(" emotions successfully detected") 
-        
+# ------------------ Recommend Songs -------------------
+if emotion_list:
+    new_df = fun(emotion_list)
+    st.markdown("<h5 style='text-align: center; color: grey;'><b>Recommended Songs with Spotify Players</b></h5>", unsafe_allow_html=True)
+    st.write("---------------------------------------------------------------------------------------------------------------------")
 
-with col3:
-    pass
-
-new_df = fun(list)
-st.write("")
-
-st.markdown("<h5 style='text-align: center; color: grey;'><b>Recommended song's with artist names</b></h5>"
-            , unsafe_allow_html=True)
-
-st.write("---------------------------------------------------------------------------------------------------------------------")
-
-try:
-  
-    for l,a,n,i in zip(new_df["link"],new_df['artist'],new_df['name'],range(30)):
-
-        st.markdown("""<h4 style='text-align: center;'><a href={}>{} - {}</a></h4>"""
-                    .format(l,i+1,n),unsafe_allow_html=True)
-        st.markdown("<h5 style='text-align: center; color: grey;'><i>{}</i></h5>" 
-                    .format(a), unsafe_allow_html=True)
-        st.write("---------------------------------------------------------------------------------------------------------------------")
-except:
-    pass
+    for name, artist in zip(new_df['name'], new_df['artist']):
+        embed_link = get_spotify_embed_link(name, artist)
+        if embed_link:
+            st.markdown(f"<h4 style='text-align: center; color: #4ecdc4'>{name} - {artist}</h4>", unsafe_allow_html=True)
+            st.markdown(f"""<iframe src="{embed_link}" width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>""", unsafe_allow_html=True)
+            st.write("---")
